dfa0f539b74a87118caadf356b106bf9c223cf53
Johny Ramos <B00139596@mytudublin.ie>
2024-02-06 20:00:09 +0000

    1st Commit. Proof of Certifcation activities.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-Epic PHP!
-=========
-
-This repository holds the screencast code, script and coding activities for
-episode 1 of the upcoming PHP series "Writing Epic PHP in one month".
-
-Contributing
-------------
-
-Progress will be posted to this repository as we work. If you'd like to contribute,
-either open up an issue or (even better) fork the code and create a pull
-request. We want to create the best PHP tutorial for beginners, a job that's
-best solved by as many people as possible :).
+## SymFonyCast PHP Beginner - Course Certification 01
+## PHP: LEVEL 1 - Branch: Course-Cert-1
+## PHP: LEVEL 2 - Branch: Course-Cert-2
+## PHP: LEVEL 3 - Branch: Course-Cert-3
+=======================
+TU Dublin -
+Course code: TU054
+Course name: Higher Certification in Computer System Management
+Module: Web Application
+Lecturer: Robert Smith
+==============================
+Student Number: B00139596
+Student Name: Johny Ramos Assuncao
+====
+## NOTES - Branch: Course-Cert-1
+This is a replication of the Laboratory of the Course Certification SymFonyCast PHP:level 1 as proof of study. 
--- index.php
+++ index.php
@@ -1,120 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="favicon.ico">
+<?php
+require 'lib/functions.php';
 
-    <title>AirPupNMeow.com: All the love, none of the crap!</title>
+$pets = get_pets();
 
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
+$pets = array_reverse($pets);
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-</head>
+$strUnkown = 'Unknown';
 
-<body>
+$cleverWelcomeMessage = 'All the love, none of the crap!';
+$pupCount = count($pets);
+?>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">AirPupNMeow</a>
-            </div>
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="navbar-form navbar-right">
-                    <div class="form-group">
-                        <input type="text" placeholder="Email" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" placeholder="Password" class="form-control">
-                    </div>
-                    <button type="submit" class="btn btn-success">Sign in</button>
-                </form>
-            </div>
-            <!--/.navbar-collapse -->
-        </div>
-    </div>
+<?php require 'layout/header.php'; ?>
 
-    <div class="jumbotron">
-        <div class="container">
-            <h1>Hello, world!</h1>
-
-            <p>This is a template for a simple marketing or informational website. It includes a large callout called the
-                hero unit and three supporting pieces of content. Use it as a starting point to create something more
-                unique.</p>
-
-            <p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
-        </div>
-    </div>
-
+<div class="jumbotron">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+        <h1><?php echo $cleverWelcomeMessage; ?></h1>
 
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                    condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-                    euismod. Donec sed odio dui. </p>
+        <p>With over <?php echo $pupCount ?> pet friends!</p>
 
-                <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                    condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-                    euismod. Donec sed odio dui. </p>
-
-                <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
-                    porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-                    fermentum massa justo sit amet risus.</p>
-
-                <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-            </div>
-        </div>
-
-        <hr>
-
-        <footer>
-            <p>&copy; AirPupNMeow.com</p>
-        </footer>
+        <p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
     </div>
-    <!-- /container -->
+</div>
 
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+<div class="container">
+    <div class="row">
+        <?php foreach ($pets as $cutePet) { ?>
+            <div class="col-md-4 pet-list-item">
+                <h2><?php echo $cutePet['name']; ?></h2>
+
+                <img src="/images/<?php echo $cutePet['image']; ?>" class="img-rounded">
+
+                <blockquote class="pet-details">
+                    <span class="label label-info"><?php echo $cutePet['breed']; ?></span>
+                    <?php
+
+                    if (!array_key_exists('age', $cutePet) || $cutePet['age'] == '') {
+                        echo $strUnkown;
+                    } elseif ($cutePet['age'] == 'hidden') {
+                        echo '(contact owner for age)';
+                    } else {
+                        echo $cutePet['age'];
+                    }
+
+                    ?>
+                    <?php echo $cutePet['weight']; ?> lbs
+                </blockquote>
+
+                <p><?php echo $cutePet['bio']; ?></p>
+            </div>
+        <?php } ?>
+
+        <?php
+        foreach ($pets as $cutePet) {
+            echo '<div class="col-lg-4">';
+            echo '<h2>';
+            echo $cutePet['name'];
+            echo '</h2>';
+            echo '<p>
+                            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+                            condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+                            euismod. Donec sed odio dui. 
+                    </p>';
+            echo '<p><a class="btn btn-default" href="#">View details &raquo;</a></p>';
+            echo '</div>';
+        }
+        ?>
+    </div>
+
+    <hr>
+    <?php 
+        require 'layout/footer.php';
+    ?>
